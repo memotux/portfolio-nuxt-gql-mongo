1c54683ee7ce5aa351bf5bb090fbc6e4ba418ff9
memoTux <romeo@mendezfuentes.net>
2023-10-09 10:43:04 -0600

    add graphiql

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,19 @@ useSeoMeta({
 
 <template>
   <AppHeader />
-  <UContainer>
+  <UContainer
+    as="main"
+    :ui="{
+      constrained: $route.path === '/graphql' ? 'max-w-[100dvw] h-[90%]' : 'max-w-7xl',
+    }"
+  >
     <slot />
   </UContainer>
   <UNotifications />
 </template>
+
+<style>
+#__nuxt {
+  @apply h-[100dvh];
+}
+</style>
--- pages/graphql.vue
+++ pages/graphql.vue
@@ -1,11 +1,42 @@
-<script lang="ts" setup>
-useSeoMeta({
+<script setup>
+useHead({
   title: 'GraphiQL | Tux Users',
+  script: [
+    { crossorigin: '', src: 'https://unpkg.com/react@18/umd/react.development.js' },
+    {
+      crossorigin: '',
+      src: 'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+    },
+    { type: 'application/javascript', src: 'https://unpkg.com/graphiql/graphiql.min.js' },
+    // { type: 'application/javascript', src: '/graphiql.js' },
+  ],
+  link: [{ rel: 'stylesheet', href: 'https://unpkg.com/graphiql/graphiql.min.css' }],
+})
+
+onMounted(() => {
+  if (window) {
+    const root = window.ReactDOM.createRoot(document.getElementById('graphiql'))
+    const fetcher = window.GraphiQL.createFetcher({
+      url: 'http://localhost:3000/api/gql',
+      // headers: { 'X-Example-Header': 'foo' },
+    })
+    // const explorerPlugin = window.GraphiQLPluginExplorer.explorerPlugin()
+    root.render(
+      window.React.createElement(window.GraphiQL, {
+        fetcher,
+        defaultEditorToolsVisibility: true,
+        // plugins: [explorerPlugin],
+      })
+    )
+  }
 })
 </script>
 
 <template>
-  <h1>Page: Graphiql</h1>
+  <div
+    id="graphiql"
+    class="h-full"
+  >
+    Loading...
+  </div>
 </template>
-
-<style scoped></style>
